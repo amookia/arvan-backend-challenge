e6bac00770d887a2372f078c4622c28dbd122a65
Kia <kiasn21@gmail.com>
2023-07-28 15:47:19 +0330

    feat: mongodb config

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,12 +4,20 @@ type (
 	Config struct {
 		Redis      Redis      `yaml:"redis"`
 		Middleware Middleware `yaml:"middleware"`
+		Mongo      Mongo      `yaml:"mongo"`
 	}
 
 	Redis struct {
 		Host string `yaml:"host"`
 		Port string `yaml:"port"`
 	}
+
+	Mongo struct {
+		Host string `yaml:"host"`
+		Port string `yaml:"port"`
+		Name string `yaml:"name"`
+	}
+
 	Middleware struct {
 		UserKey   string `yaml:"userkey"`
 		PerMinute int    `yaml:"perminute"`
